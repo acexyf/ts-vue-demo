a9128c8dfc7e0c0a5f39017c87415c98047366df
谢小飞 <corner_scofield@163.com>
2021-10-01 20:21:17 +0800

    add

--- src/components/MyInput.vue
+++ src/components/MyInput.vue
@@ -1,12 +1,30 @@
 <template>
-  <div></div>
+  <div>
+    <div>v-model: {{ val }}</div>
+    <div @click="clickBtn">myinput</div>
+    <input type="text" :value="val" @input="changeInput" />
+  </div>
 </template>
 
-<script>
-import { Component, Model, Vue } from "vue-property-decorator";
+<script lang="ts">
+import { Component, Emit, Model, Vue } from "vue-property-decorator";
 
 @Component
-export default class MyInput extends Vue {}
+export default class MyInput extends Vue {
+  private item = { id: 3 };
+
+  @Model("input", { type: String }) readonly val!: string;
+
+  @Emit("input")
+  clickBtn() {
+    return "new eeee";
+  }
+
+  @Emit("input")
+  changeInput(ev: any) {
+    return ev.target.value;
+  }
+}
 </script>
 
 <style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" @click="clickBtn" />
-    <div>{{ fullName | filterName }}</div>
+    <div ref="refDiv">{{ fullName | filterName }}</div>
     <HelloWorld
       msg="sssWelcome to Your Vue.js + TypeScript App"
       :msg1.sync="firtName"
     />
-    <MyInput v-model="namess" />
+    <div>namess:{{ namess }}</div>
+    <MyInput v-model="namess" @click-btn="emitFn" ref="inputRef" />
+    <div @click="clickSubmit">submit</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Ref, Vue, Watch } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 import MyInput from "@/components/MyInput.vue";
 Component.registerHooks(["beforeRouteLeave", "beforeRouteEnter"]);
@@ -28,6 +30,9 @@ Component.registerHooks(["beforeRouteLeave", "beforeRouteEnter"]);
   },
 })
 export default class Home extends Vue {
+  @Ref() readonly refDiv!: HTMLElement;
+  @Ref("refDiv") readonly newRef!: HTMLElement;
+  @Ref() readonly inputRef!: MyInput;
   mounted() {
     console.log(1333311);
     // eslint-disable-next-line
@@ -43,7 +48,7 @@ export default class Home extends Vue {
   }
   private firtName = "tom";
   private lastName = "bluce";
-  private namess = "";
+  private namess = "eee";
 
   get fullName(): string {
     console.log("getter");
@@ -54,6 +59,16 @@ export default class Home extends Vue {
   @Watch("firtName")
   changeFirstName(val: string, oldVal: string) {
     console.log(val);
+  }
+
+  emitFn(item: any, index: number): void {
+    console.log("parent emit", item, index);
+  }
+
+  clickSubmit(): void {
+    console.log(this.refDiv);
+    console.log(this.newRef);
+    
   }
 }
 </script>
